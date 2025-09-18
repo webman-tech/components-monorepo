16877567c4320647da1805e7fb930c4655282d09
kriss <462679766@qq.com>
2025-09-18 20:24:36 +0800

    feature: BaseResponseDTO 支持 headers 和 status

--- tests/Unit/DTO/BaseResponseDTOTest.php
+++ tests/Unit/DTO/BaseResponseDTOTest.php
@@ -4,8 +4,8 @@ use Webman\Http\Response;
 use WebmanTech\DTO\BaseResponseDTO;
 use WebmanTech\DTO\Helper\ConfigHelper;
 
-test('toResponse with format', function () {
-    class DTOToResponseWithFormat extends BaseResponseDTO
+test('不同 format', function () {
+    class DTOToResponseForFormat extends BaseResponseDTO
     {
         public function __construct(
             public readonly string $name,
@@ -15,7 +15,7 @@ test('toResponse with format', function () {
     }
 
     // 默认 json
-    $dto = new DTOToResponseWithFormat(name: 'nameValue');
+    $dto = new DTOToResponseForFormat(name: 'nameValue');
     $response = $dto->toResponse();
     expect($response)->toBeInstanceOf(Response::class)
         ->and($response->getHeader('Content-Type'))->toBe('application/json');
@@ -24,11 +24,43 @@ test('toResponse with format', function () {
     ConfigHelper::setForTest('dto.to_response_format', function (array $data) {
         return response(json_encode($data), 201);
     });
-    $dto = new DTOToResponseWithFormat(name: 'nameValue');
+    $dto = new DTOToResponseForFormat(name: 'nameValue');
     $response = $dto->toResponse();
     expect($response)->toBeInstanceOf(Response::class)
         ->and($response->getStatusCode())->toBe(201);
 
     // 重置
     ConfigHelper::setForTest();
+});
+
+test('json 下返回空对象时', function () {
+    class DTOToResponseEmpty extends BaseResponseDTO
+    {
+    }
+
+    $dto = new DTOToResponseEmpty();
+    $response = $dto->toResponse();
+    expect($response)->toBeInstanceOf(Response::class)
+        ->and($response->rawBody())->toBe('{}');
+});
+
+test('传递 headers 和 status', function () {
+    class DTOToResponseForHeadersAndStatus extends BaseResponseDTO
+    {
+        public function __construct(
+            public readonly string $name,
+        )
+        {
+        }
+    }
+
+    // 默认 json
+    $dto = new DTOToResponseForHeadersAndStatus(name: 'nameValue');
+    $dto->withResponseStatus(201)
+        ->withResponseHeaders(['X-Test' => 'test']);
+    $response = $dto->toResponse();
+    expect($response)->toBeInstanceOf(Response::class)
+        ->and($response->getHeader('Content-Type'))->toBe('application/json')
+        ->and($response->getHeader('X-Test'))->toBe('test')
+        ->and($response->getStatusCode())->toBe(201);
 });
