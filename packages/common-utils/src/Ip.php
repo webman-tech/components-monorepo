02d060fa103cf9dd2e589e7bfece4e0ae23eb360
kriss <462679766@qq.com>
2025-12-19 13:51:43 +0800

    feature(common-utils): 提供 Ip 的工具类

---
+++ packages/common-utils/src/Ip.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace WebmanTech\CommonUtils;
+
+use Webman\Http\Request as WebmanRequest;
+
+final class Ip
+{
+    /**
+     * 判断是否内网 IP
+     */
+    public static function isIntranetIp(string $ip): bool
+    {
+        if (class_exists(WebmanRequest::class)) {
+            return WebmanRequest::isIntranetIp($ip);
+        }
+
+        // 以下代码是从 WebmanRequest::isIntranetIp 复制而来
+
+        // Not validate ip .
+        if (!filter_var($ip, FILTER_VALIDATE_IP)) {
+            return false;
+        }
+        // Is intranet ip ? For IPv4, the result of false may not be accurate, so we need to check it manually later .
+        if (!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
+            return true;
+        }
+        // Manual check only for IPv4 .
+        if (!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+            return false;
+        }
+        // Manual check .
+        $reservedIps = [
+            1681915904 => 1686110207, // 100.64.0.0 -  100.127.255.255
+            3221225472 => 3221225727, // 192.0.0.0 - 192.0.0.255
+            3221225984 => 3221226239, // 192.0.2.0 - 192.0.2.255
+            3227017984 => 3227018239, // 192.88.99.0 - 192.88.99.255
+            3323068416 => 3323199487, // 198.18.0.0 - 198.19.255.255
+            3325256704 => 3325256959, // 198.51.100.0 - 198.51.100.255
+            3405803776 => 3405804031, // 203.0.113.0 - 203.0.113.255
+            3758096384 => 4026531839, // 224.0.0.0 - 239.255.255.255
+        ];
+        $ipLong = ip2long($ip);
+        foreach ($reservedIps as $ipStart => $ipEnd) {
+            if (($ipLong >= $ipStart) && ($ipLong <= $ipEnd)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
